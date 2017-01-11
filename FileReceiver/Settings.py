7af2088045e4bdba0d4f95f1ebb554b091b612e5
Andrew Plotnikov <x-softer@mail.ru>
2017-01-11 14:29:26 +0300

    test2

--- FileReceiver/Settings.py
+++ FileReceiver/Settings.py
@@ -3,5 +3,5 @@ HOSTNAME =  "localhost"
 
 LOG_PATH = "logs"
 STORE_FILES_PATH = r"d:\temp\r"
-PRINT_LOG_TO_CONSOLE = True
-#PRINT_LOG_TO_CONSOLE = False
+#PRINT_LOG_TO_CONSOLE = True
+PRINT_LOG_TO_CONSOLE = False
